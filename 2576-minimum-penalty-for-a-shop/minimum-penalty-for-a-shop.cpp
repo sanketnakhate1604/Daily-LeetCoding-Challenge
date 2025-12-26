5415ec5f78911568caa5f850be38e696042afb5e
sanket <sanketnakhate1604@gmail.com>
2025-12-26 10:52:37 +0530

    Time: 4 ms (81.86%) | Memory: 13.3 MB (91.48%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int totalY = 0;
+        for (char c : customers)
+            if (c == 'Y') totalY++;
+
+        int penalty = totalY;
+        int minPenalty = penalty;
+        int bestHour = 0;
+
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers[i] == 'Y')
+                penalty--;
+            else
+                penalty++;
+
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                bestHour = i + 1;
+            }
+        }
+
+        return bestHour;
+    }
+};
