4cb910e03e876a855e85b3939cfc9678882dceb1
sanket <sanketnakhate1604@gmail.com>
2026-01-04 21:33:11 +0530

    Time: 12 ms (30.63%) | Memory: 7.7 MB (87.16%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int  f(int n){
+        if(n==0||n==1){
+            return n;
+        }
+
+        return f(n-1)+f(n-2);
+    }
+    int fib(int n) {
+        
+        return f(n);
+
+        
+    }
+};
