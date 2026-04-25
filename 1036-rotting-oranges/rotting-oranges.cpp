c6637c4c116dbf65a56da15eb267907d9a444568
sanket <sanketnakhate1604@gmail.com>
2026-04-25 18:44:22 +0530

    Time: 4 ms (15.32%) | Memory: 16.8 MB (69.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,68 +3,64 @@ public:
     int x[4]={-1,1,0,0};
     int y[4]={0,0,-1,1};
 
-    bool valid(int i,int j,int n,int m){
-        if(i<0 || i>=n || j<0||j>=m){
+    bool valid(int i ,int j,int n,int m){
+        if(i<0||i>=n||j<0||j>=m){
             return false;
 
         }
         return true;
     }
-
-
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-
+    int orangesRotting(vector<vector<int>>& a) {
+        int n=a.size();
+        int m=a[0].size();
         int time=0;
         int fresh=0;
-        
+
         queue<pair<int,int>>q;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
+                if(a[i][j]==2){
                     q.push({i,j});
-                    grid[i][j]=-1;
+                    a[i][j]=-1;
                 }
-                else if(grid[i][j]==1){
+                else if(a[i][j]==1){
                     fresh++;
                 }
             }
         }
-
 
         while(q.empty()==0 && fresh>0){
             time++;
             int s=q.size();
 
             while(s--){
-               pair<int,int>p=q.front();
-               q.pop();
+                pair<int,int>p=q.front();
+                q.pop();
 
-               int r=p.first;
-               int c =p.second;
+                int r=p.first;
+                int c=p.second;
 
-               for(int k=0;k<4;k++){
-                 int row=r+x[k];
-                 int col=c+y[k];
+                for(int k=0;k<4;k++){
+                    int row=r+x[k];
+                    int col=c+y[k];
 
-
-                 if(valid(row,col,n,m)  && grid[row][col]==1){
-                    fresh--;
-                    q.push({row,col});
-                    grid[row][col]=-1;
-                 }
-               }
-
-               
+                    if(valid(row,col,n,m)&&a[row][col]==1){
+                         fresh--;
+                         q.push({row,col});
+                         a[row][col]=-1;
+                    }
+                }
             }
         }
+
         if(fresh>0){
             return -1;
         }
 
         return time;
 
-         
+
+        
     }
 };
