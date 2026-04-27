5f95c4b245797377b48b3f52fcaed71df5ec55c7
sanket <sanketnakhate1604@gmail.com>
2026-04-27 15:10:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (20.17%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -5,47 +5,43 @@ public:
 
     bool valid(int i,int j,int n,int m){
         if(i<0||i>=n||j<0||j>=m){
-            return  false;
+            return false;
         }
-
         return true;
     }
-    void dfs(vector<vector<int>>& a,int i,int j,int n,int m,int color,int original,vector<vector<bool>>&vis, vector<vector<int>>&res){
-        vis[i][j]=1;
-        
-
-        for(int k=0;k<4;k++){
-            int row=i+x[k];
-            int col=j+y[k];
-
-            if(valid(row,col,n,m)&& a[row][col]==original && vis[row][col]==0){
-                // ab colur kar or fhir se dfs call kar de
-                res[row][col]=color;
-                dfs(a,row,col,n,m,color,original,vis,res);
-
-            }
-        }
-
-        return ;
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& a, int i, int j, int color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& a, int src, int dest, int color) {
         int n=a.size();
         int m=a[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,0));
+        int element=a[src][dest];
+        queue<pair<int,int>>q;
 
-        
-
-        vector<vector<int>>res=a;
-
+        q.push({src,dest});
+        a[src][dest]=color;
+        vector<vector<bool>>vis(n);
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                res[i][j]=a[i][j];
-            }
+            vector<bool>t(m,0);
+            vis[i]=t;
         }
-         int original=a[i][j];
-         res[i][j]=color;
-         dfs(a,i,j,n,m,color,original,vis,res);
+        vis[src][dest]=1;
 
-        return res;
+        while(q.empty()==0){
+           pair<int,int>p=q.front();
+           q.pop();
+
+           int r=p.first;
+           int c=p.second;
+
+           for(int k=0;k<4;k++){
+            int row=r+x[k];
+            int col=c+y[k];
+             if(valid(row,col,n,m)&&a[row][col]==element&&vis[row][col]==0){
+                q.push({row,col});
+                vis[row][col]=1;
+                a[row][col]=color;
+             }
+           }
+        }
+
+        return a;
     }
 };
